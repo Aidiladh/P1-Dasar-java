c887a035f791362e31116d41dd940783ab3c9cd7
Aidiladh <aidila1684211@gmail.com>
2022-03-14 15:44:49 +0700

    penamaan variabl (identifier)

---
+++ src/C/VAriabel.java
@@ -0,0 +1,14 @@
+package C;
+/*
+//penamaan variabl (identifier)
+/*
+ */
+public class VAriabel
+{
+    public static void main(String[] args) {
+        double salary;
+        long distance;
+        boolean $flag;
+
+    }
+}
