6eeada0129bc583d936ac0b445479d8d1f65baa1
Aidiladh <aidila1684211@gmail.com>
2022-03-14 20:03:17 +0700

    //mencetak literal karakter

---
+++ src/LinteralChar.java
@@ -0,0 +1,15 @@
+//mencetak literal karakter
+public class LinteralChar {
+    public static void main(String[] args) {
+
+        char tab = '\t';
+        char newline = '\n';
+        char singelQoute = '\"';
+        char doubleQoute = '\"';
+        System.out.println("ini contoh" + tab + "literal tab");
+        System.out.println("ini contoh" + newline + "literal newline");
+        System.out.println(""+singelQoute+"ini contoh literal singel qoute"+singelQoute);
+        System.out.println(""+doubleQoute+"ini contoh literal doubleQoute"+doubleQoute);
+
+    }
+}
