7f7e542c59fb68f785ff363e7ef995345555a80f
Aidiladh <aidila1684211@gmail.com>
2022-03-14 17:23:25 +0700

    menampilkan bilangan octal ke format desimal

---
+++ src/Octal.java
@@ -0,0 +1,17 @@
+//menampilkan bilangan octal ke format desimal
+public class Octal {
+    public static void main(String[] args) {
+        int six=06;
+        int seven=07;
+        int eight=010;
+        int nine=011;
+        int twenty=014;
+        System.out.println("octal six ="+six);
+        System.out.println("octal seven ="+seven);
+        System.out.println("octal eight ="+eight);
+        System.out.println("octal nine ="+nine);
+        System.out.println("octal twenty ="+twenty);
+
+
+    }
+}
